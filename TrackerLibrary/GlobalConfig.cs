5ed61d72a53a8c293e248778da4f3312dae9d6d9
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-10-26 16:11:56 +0000

    Added connection classes & config classes

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - create the SQL connection
+
+            }
+
+            if (textFiles)
+            {
+                // TODO - create the text connection
+
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/TextConnector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class TextConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
