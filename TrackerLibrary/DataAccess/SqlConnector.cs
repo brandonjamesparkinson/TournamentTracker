dda9f913654921f558e5624a3608309126a00384
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-10-27 15:02:14 +0000

    able to create prize in db | added upload logic to use stored procedure | fixed app.config connection settings

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,5 +1,12 @@
-﻿using System.Data;
+﻿using Dapper;
+using System.Data;
 using TrackerLibrary.Models;
+
+	//@PlaceNumber int,
+	//@PlaceName nvarchar(50),
+	//@PrizeAmount money,
+    //@PrizePercentage decimal (6,3),
+	//@id int = 0 output
 
 namespace TrackerLibrary.DataAccess
 {
@@ -15,7 +22,19 @@ namespace TrackerLibrary.DataAccess
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments"))) 
             {
+                var p = new DynamicParameters();
 
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
             }
         }
     }
